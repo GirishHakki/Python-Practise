4456e208cb380299f0ee82245dcb4b37cb640a21
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-02-22 06:33:43 +0530

    Add files via upload

---
+++ programs/Count_the_number_of_each_vowel.py
@@ -0,0 +1,20 @@
+# a = "Harry Potter and the Prisoner of Azkaban"
+# vowels = "aeiou"
+# a = a.casefold()
+# print(a)
+# count = {}.fromkeys(vowels, 0)
+#
+# for char in a:
+#     if char in count:
+#         count[char]+=1
+#
+# print(count)
+
+
+# solution 2 using List and Dictonary comprehension
+a = "Harry Potter and the Prisoner of Azkaban"
+vowels = "aeiou"
+a = a.casefold()
+
+count = {key:sum([1 for char in a if char == key])for key in vowels}
+print(count)
