73acf3a8bef01d7d392a8576e0236be37b83ba75
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-03-01 10:57:53 +0530

    Add files via upload

---
+++ programs/Check_if_a_list_is_empty.py
@@ -0,0 +1,15 @@
+# solution 1 using boolean operation
+
+# my_list = []
+# if not my_list:
+#     print("the list is empty")
+
+# solution 2 using len() function
+# my_list = []
+# if len(my_list)==0:
+#     print("the list is empty")
+
+# solution 2 using []
+my_list = [9,15,20,32,44,58]
+if my_list ==[]:
+    print("the list is empty")
