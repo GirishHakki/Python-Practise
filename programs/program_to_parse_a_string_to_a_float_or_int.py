c797a76190a64d6f9ea4b475a43cce85ec6d4605
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-03-09 19:50:33 +0530

    Add files via upload

---
+++ programs/program_to_parse_a_string_to_a_float_or_int.py
@@ -0,0 +1,17 @@
+# solution 1 parse string into integer
+
+# string = "12345"
+# print (type(string))
+# int_str = int(string)
+# print((type(int_str)))
+
+# solution 2 parse string into float
+
+string = "123.45"
+print (type(string))
+print(string)
+float_string = float(string)
+print((type(float_string)))
+print(float_string)
+
+
