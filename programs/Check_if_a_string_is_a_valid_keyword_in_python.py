6d7e51e0b904948811d48cf2996a2450bbfbacc7
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-03-14 07:41:13 +0530

    Add files via upload

---
+++ programs/Check_if_a_string_is_a_valid_keyword_in_python.py
@@ -0,0 +1,9 @@
+import keyword
+print(keyword.kwlist)
+# words = ["break","john","three","continue","lambda","global","peter","for","while","number"]
+#
+# for i in range(len(words)):
+#     if keyword.iskeyword(words[i]):
+#         print(words[i],"is a keyword in python")
+#     else:
+#         print(words[i],"is not a keyword in python")
