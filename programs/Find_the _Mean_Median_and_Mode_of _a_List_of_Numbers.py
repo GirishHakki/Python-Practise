6d4ee682bef33fbaf64f53c0422f2758f96546e5
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-04 11:35:32 +0530

    Add files via upload

---
+++ programs/Find_the _Mean_Median_and_Mode_of _a_List_of_Numbers.py
@@ -0,0 +1,11 @@
+from statistics import mean, median, mode
+
+my_list = [1, 2, 2, 3, 4, 4, 4, 5]
+
+mean_value = mean(my_list)
+median_value = median(my_list)
+mode_value = mode(my_list)
+
+print("Mean:", mean_value)
+print("Median:", median_value)
+print("Mode:", mode_value)
