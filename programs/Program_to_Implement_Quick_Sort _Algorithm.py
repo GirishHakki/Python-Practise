a17295e3d901991afd139008d82d9e8a39579ad7
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-10 06:55:30 +0530

    Add files via upload

---
+++ programs/Program_to_Implement_Quick_Sort _Algorithm.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+my_array = [64, 25, 12, 22, 11]
+my_array = quick_sort(my_array)
+print("Sorted array using Quick Sort:", my_array)
