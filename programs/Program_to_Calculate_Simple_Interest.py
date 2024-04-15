2d8e07cf1444f5deef34dd7fc2c5d4f92b544bce
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-15 06:51:57 +0530

    Add files via upload

---
+++ programs/Program_to_Calculate_Simple_Interest.py
@@ -0,0 +1,7 @@
+def simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+
+principal = 1000
+rate = 5
+time = 2
+print("Simple Interest:", simple_interest(principal, rate, time))
