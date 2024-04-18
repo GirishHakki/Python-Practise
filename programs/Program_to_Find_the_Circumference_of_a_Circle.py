5a60ac80c35f84a8e9bd350c8f5f300d225fba1d
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-18 06:01:32 +0530

    Add files via upload

---
+++ programs/Program_to_Find_the_Circumference_of_a_Circle.py
@@ -0,0 +1,6 @@
+import math
+def circumference_of_circle(radius):
+    return 2 * math.pi * radius
+
+radius = 5
+print("Circumference of the circle with radius", radius, "is:", circumference_of_circle(radius))
