33ec400885eb16db5664c7ad9e4f95044d998fc4
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-19 12:08:24 +0530

    Add files via upload

---
+++ programs/Program_to_Calculate_the_Area_of_a_Rectangle.py
@@ -0,0 +1,6 @@
+def area_of_rectangle(length, width):
+    return length * width
+
+length = 5
+width = 4
+print("Area of the rectangle with length", length, "and width", width, "is:", area_of_rectangle(length, width))
