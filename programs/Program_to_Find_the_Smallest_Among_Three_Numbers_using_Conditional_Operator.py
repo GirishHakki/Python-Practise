e65fa6e4de9c786f4a313fe90b42ca60c9f9c20b
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-04-23 06:10:22 +0530

    Add files via upload

---
+++ programs/Program_to_Find_the_Smallest_Among_Three_Numbers_using_Conditional_Operator.py
@@ -0,0 +1,7 @@
+def find_smallest(num1, num2, num3):
+    return min(num1, num2, num3)
+
+num1 = 10
+num2 = 5
+num3 = 7
+print("Smallest number among", num1, ",", num2, ", and", num3, "is:", find_smallest(num1, num2, num3))
