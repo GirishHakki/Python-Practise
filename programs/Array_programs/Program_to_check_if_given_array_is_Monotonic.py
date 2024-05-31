f1afe14056ba836eb6553f0b8f3c3e2fe076c8c4
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-05-31 10:58:10 +0530

    Add files via upload

---
+++ programs/Array_programs/Program_to_check_if_given_array_is_Monotonic.py
@@ -0,0 +1,33 @@
+# Check if given array is Monotonic
+def isMonotonic(A):
+    x, y = [], []
+    x.extend(A)
+    y.extend(A)
+    x.sort()
+    y.sort(reverse=True)
+    if(x == A or y == A):
+        return True
+    return False
+
+
+# Driver program
+A = [6, 5, 4, 4]
+
+# Print required result
+print(isMonotonic(A))
+
+
+#Below is the implementation of the above approach:
+
+# Python Program to check if given array is Monotonic
+# Check if given array is Monotonic
+
+def isMonotonic(A):
+    return (all(A[i] <= A[i + 1] for i in range(len(A) - 1)) or
+            all(A[i] >= A[i + 1] for i in range(len(A) - 1)))
+
+# Driver program
+A = [6, 5, 4, 4]
+
+# Print required result
+print(isMonotonic(A))
