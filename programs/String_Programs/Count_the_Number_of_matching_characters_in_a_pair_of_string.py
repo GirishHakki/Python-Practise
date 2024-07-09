1156045a14fff37c0f989c3d6b3c9270ad7d2cef
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-07-09 22:29:31 +0530

    Add files via upload

---
+++ programs/String_Programs/Count_the_Number_of_matching_characters_in_a_pair_of_string.py
@@ -0,0 +1,12 @@
+def commonfun(str1, str2):
+    return (len((set(str1)).intersection(set(str2))))
+
+
+# string1=input("Enter String 1 : ")
+string1 = "VISHAl"
+string2 = "VANSHIKA"
+# string2=input("Enter String 2 : ")
+
+no_of_common_character = commonfun(string1.lower(), string2.lower())
+
+print("NO. OF COMMON CHRACTERS ARE : ", no_of_common_character)
