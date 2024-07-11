c2929247fb0c8d9e8963d1417d84f5b17f9138e3
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-07-11 22:16:09 +0530

    Add files via upload

---
+++ programs/String_Programs/Least_Frequent_Character_in_String.py
@@ -0,0 +1,18 @@
+# initializing string
+test_str = "GirishHakki"
+
+# printing original string
+print ("The original string is : " + test_str)
+
+# using naive method to get
+# Least Frequent Character in String
+all_freq = {}
+for i in test_str:
+    if i in all_freq:
+        all_freq[i] += 1
+    else:
+        all_freq[i] = 1
+res = min(all_freq, key = all_freq.get)
+
+# printing result
+print ("The minimum of all characters in GirishHakki is : " + str(res))
