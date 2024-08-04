704da8dcac79f94ec1f309d8fe417f4da3a30ece
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-08-04 06:39:26 +0530

    Add files via upload

---
+++ programs/String_Programs/Replace_all_occurrences_of_a_substring_in_a_string.py
@@ -0,0 +1,6 @@
+#Python has inbuilt function replace to replace all occurrences of substring.
+input_string = "Hello World"
+s1 = "World"
+s2 = "Girish"
+input_string = input_string.replace(s1, s2)
+print(input_string)
